e713babed55637aec5ed85a5c9362967f9db2d71
Paulo Daniel Ambrosio dos Santos <pda.ambrosio@gmail.com>
2022-03-10 21:07:38 -0300

    Delete owncloud directory

--- owncloud/apps/federation/l10n/es_AR.js
+++
@@ -1,7 +0,0 @@
-OC.L10N.register(
-    "federation",
-    {
-    "Add" : "Agregar",
-    "Server added to the list of trusted ownClouds" : "Servidor añadido a la lista de confianza de ownCloud"
-},
-"nplurals=2; plural=(n != 1);");
--- owncloud/apps/files_trashbin/l10n/hi.js
+++
@@ -1,7 +0,0 @@
-OC.L10N.register(
-    "files_trashbin",
-    {
-    "Error" : "त्रुटि",
-    "Delete" : "हटाना"
-},
-"nplurals=2; plural=(n != 1);");
